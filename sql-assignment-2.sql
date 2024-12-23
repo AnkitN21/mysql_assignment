93c6229ddd8ae9541cf6f61ebf4712eba90470b5
AnkitN21 <ankitnegi.asn21@gmail.com>
2024-12-23 16:39:31 +0530

    assignment 2 added

--- sql-assignment-2.sql
+++ sql-assignment-2.sql
@@ -1,22 +1,46 @@
 select * from company.worker;
 
--- Q-1. Write an SQL query to print the first three characters of  FIRST_NAME from Worker table.
+-- Q-1. Write an SQL query to print the first three characters of FIRST_NAME from Worker table.
 
-select
-	substring(first_name, 1,3) as first3char
+select *,
+	substring(first_name, 1,3) as first_name
 from company.worker;
 
 -- Q-2. Write an SQL query to find the position of the alphabet (‘a’) in the first name
--- column ‘Amitabh’ from Worker table.
+-- column ‘minakshi’ from Worker table.
 
 select 
-	instr(first_name, 'a') as postion
+	position("i" in first_name) as position
+    -- instr(substring(first_name,3,10), 'i') + 2 as postion
 from company.worker
-where first_name = 'amitabh';
-
--- Q-3. Write an SQL query to print the name of employees having the highest salary in each
+where first_name = 'minakshi';
 
 select
-	max(salary) as highsal_dept
+	locate("i",
+		substring(first_name,
+			locate("i",first_name)+1, 
+				length(first_name))) + locate("i", first_name) as position
 from company.worker
-group by department;
+where first_name= "minakshi";
+
+-- Q-3. Write an SQL query to print the name of employees having the highest salary and 
+-- highest salary in each department.
+
+select
+	concat(first_name," ",last_name) as full_name, salary, department
+from company.worker
+where(department, salary) in (
+	select department, max(salary)
+    from company.worker
+    group by department
+    );
+
+select 	
+	first_name,
+    salary,
+    department
+from company.worker as w1
+where salary = (
+			select max(salary)
+            from company.worker as w2
+            where w1.department = w2.department);
